36cec270e81aeab57db5a20f0174ceba273378de
Mark Boddington <gitlab@badpenguin.co.uk>
2021-11-19 15:05:05 +0000

    Update stored_cc_files_bak.php

--- MainApp/stored_cc_files_bak.php
+++ MainApp/stored_cc_files_bak.php
@@ -16,7 +16,19 @@
 	
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+	if (isset($_SERVER['HTTP_X_MESH_REQUEST_ID'])) {
+		$random_spanid = bin2hex( random_bytes(8) );
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			"X-Mesh-Request-ID: " . $_SERVER['HTTP_X_MESH_REQUEST_ID'],
+			"X-B3-TraceId: " . $_SERVER['HTTP_X_B3_TRACEID'],
+			"X-B3-SpanId: " . $random_spanid,
+			"X-B3-ParentSpanId: " . $_SERVER['HTTP_X_B3_SPANID'],
+			"X-B3-Sampled: " . $_SERVER['HTTP_X_B3_SAMPLED'],
+			'Content-Type: application/json'
+		));
+	} else {
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+	}
 	$result = curl_exec($ch);
 	$info = curl_getinfo($ch);
 	curl_close($ch);
