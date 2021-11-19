022c5ab3f4bb1171e920a342b935f45537fa451f
Mark Boddington <gitlab@badpenguin.co.uk>
2021-11-19 15:13:53 +0000

    content-type

--- MainApp/trading/reference.php
+++ MainApp/trading/reference.php
@@ -28,7 +28,8 @@
 			"X-B3-TraceId: " . $_SERVER['HTTP_X_B3_TRACEID'],
 			"X-B3-SpanId: " . $random_spanid,
 			"X-B3-ParentSpanId: " . $_SERVER['HTTP_X_B3_SPANID'],
-			"X-B3-Sampled: " . $_SERVER['HTTP_X_B3_SAMPLED']
+			"X-B3-Sampled: " . $_SERVER['HTTP_X_B3_SAMPLED'],
+			'Content-Type: application/json'
 		));
 	} else {
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
